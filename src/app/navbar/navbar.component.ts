1fc8b7850ec6974bf50e0f8f823c1812e13b3e28
AndersonBillss <anderson@thebillsfamily.com>
2024-10-10 10:36:21 -0600

    Show page on navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,11 +10,22 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     RouterModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    RouterModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
-
+export class NavbarComponent implements OnInit {
+  constructor(
+    private _router: Router
+  ){}
+  currentPage: string = ''
+  ngOnInit(): void {
+    this._router.events.subscribe((event) => {
+      if (event instanceof(NavigationEnd)){
+        this.currentPage = event.url.slice(1,event.url.length)
+      }
+    })
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { ParallaxBackgroundComponent } from '../../shared/parallax-background/parallax-background.component';
+import { ParallaxBackgroundComponent } from "../../shared/parallax-background/parallax-background.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    ParallaxBackgroundComponent
-  ],
+  imports: [ParallaxBackgroundComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
