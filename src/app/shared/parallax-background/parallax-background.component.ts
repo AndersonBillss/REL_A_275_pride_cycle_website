81a371b837b7e4fdbd37baeecf65a6843d9f9150
AndersonBillss <anderson@thebillsfamily.com>
2024-10-10 11:57:29 -0600

    added factor for parallax background

--- src/app/shared/parallax-background/parallax-background.component.ts
+++ src/app/shared/parallax-background/parallax-background.component.ts
@@ -10,48 +10,50 @@ import { Component, ViewChild, ElementRef, Input } from '@angular/core';
 export class ParallaxBackgroundComponent {
   @ViewChild('parallaxImg') parallaxImg!: ElementRef;
   @ViewChild('parallaxContainer') parallaxContainer!: ElementRef;
-
   @Input() src!: string;
   @Input() height!: number;
-  @Input() fadeTransitionSeconds?: number = 0;
+  @Input() fadeTransitionSeconds: number = 0;
+  @Input() factor: number = 2;
 
   opacity: string = "0%"
-
   prevWindowWidth: number = window.innerWidth
   imgCenterX: number = 0
   imgCenterY: number = 0
 
   ngAfterViewInit() {
+    //set up elements
     const img = this.parallaxImg.nativeElement
     const container = this.parallaxContainer.nativeElement
     container.style.height = `${this.height}px`    
+    //add event listeners
     window.addEventListener('resize', () => this.handleWindowResize(img, container, false))
     document.addEventListener('scroll', () => this.parallaxScroll(img, container))
-
+    //snap the image to the correct place
     this.handleWindowResize(img, container, true)
   }
 
 
   handleWindowResize(img: HTMLElement, container: HTMLElement, force: boolean){
+    //prevent the image from resizing if address bar comes up on mobile devices
     if((this.prevWindowWidth != window.innerWidth) || force){
       const imgHeightToWidth = img.offsetHeight / img.offsetWidth
-      
-      const imgMinHeightRequired = container.offsetHeight / 2 + window.innerHeight / 2
-      const imgRequiredWidthToHeightRatio = imgMinHeightRequired / container.offsetWidth
+      const containerHeightRequired = container.offsetHeight / 2 + window.innerHeight / 2
+      const imgRequiredHeightToWidthRatio = containerHeightRequired / container.offsetWidth
 
-      if(imgHeightToWidth > imgRequiredWidthToHeightRatio){
-        this.imgCenterY = (img.offsetHeight - imgMinHeightRequired) / 2
+      if(imgHeightToWidth > imgRequiredHeightToWidthRatio){
+        //if the image is taller than required
+        this.imgCenterY = (img.offsetHeight - containerHeightRequired) / 2
         this.imgCenterX = 0
         img.style.height = 'auto'
         img.style.width = `${container.offsetWidth}px`
-
       } else {
+        //if the image is wider than required
         this.imgCenterY = 0
         this.imgCenterX = (img.offsetWidth - container.offsetWidth) / 2
-        img.style.height = `${imgMinHeightRequired}px`
+        img.style.height = `${containerHeightRequired}px`
         img.style.width = 'auto'
-
       }
+      //appropriately set the image to the correct positionn
       this.parallaxScroll(img, container)
     }
     this.prevWindowWidth = window.innerWidth
@@ -60,10 +62,11 @@ export class ParallaxBackgroundComponent {
   parallaxScroll(img: HTMLElement, container: HTMLElement){
     const scrollY = window.scrollY
     const containerTop = container.offsetTop
-    const translation = scrollY/2 - containerTop/2
+    const translation = scrollY/this.factor - containerTop/this.factor
     img.style.transform = `translate(-${this.imgCenterX}px, ${translation - this.imgCenterY}px)`
   }
 
+  //the image only appears after it loads so that you can't see it snapping into place
   imgLoad(img: HTMLElement, container: HTMLElement){
     this.handleWindowResize(img, container, true)
     this.opacity = "100%"
